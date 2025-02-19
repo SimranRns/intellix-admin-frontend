3e51876ee816630cf53f163d089dc4f0d23b297a
SimranRns <you@example.com>
2025-02-19 11:24:41 +0530

    create project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import Button from './component/Button'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <Button />
+    </>
+  )
+}
+
+export default App
---
+++ src/component/Button.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function Button() {
+
+
+  return (
+    <>
+    <h3>Hello world</h3>
+    </>
+  )
+}
+
+export default Button
