26bc11730baa10ca136c5a46d527b4ddf1e71cb6
Payalyyadav <164471549+Payalyyadav@users.noreply.github.com>
2025-03-10 11:20:30 +0530

    Merge branch 'dev' of https://github.com/SimranRns/intellix-admin-frontend into payal

--- src/component/Admin/Advertisment/Advertisment.jsx
+++ src/component/Admin/Advertisment/Advertisment.jsx
@@ -15,8 +15,7 @@ import {
     SidebarTrigger,
 } from "../../src/components/ui/sidebar";
 import Header from "../Dashboard/Header";
-import { Button } from "@headlessui/react";
-import { Separator } from '@radix-ui/react-dropdown-menu';
+// import { Button } from "@headlessui/react";
 
 const Advertisment = () => {
     const [activeTab, setActiveTab] = useState("tab1");
