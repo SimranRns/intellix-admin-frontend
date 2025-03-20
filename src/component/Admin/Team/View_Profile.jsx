da8e508a63de47e5a42697900034324e1c6669b5
ankit-jangir <ankitjangir5826@gmail.com>
2025-03-20 18:01:26 +0530

    new

--- src/component/Admin/Team/View_Profile.jsx
+++ src/component/Admin/Team/View_Profile.jsx
@@ -40,7 +40,7 @@ const View_Profile = () => {
         firstname: "",
         lastname: "",
         enrolllmentid: "",
-        salary: "",
+        salary: "",    
         dob: "",
         gender: "",
         highqualify: "",
