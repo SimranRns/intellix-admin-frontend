ce2568dc4a141e9d79e150edfe137bd9fbd6bba3
ankit-jangir <ankitjangir5826@gmail.com>
2025-03-29 18:55:33 +0530

    new

--- src/component/Admin/Accounts/Account.jsx
+++ src/component/Admin/Accounts/Account.jsx
@@ -8,13 +8,14 @@ const Account = () => {
 
 
         <SidebarProvider style={{ "--sidebar-width": "15rem" }}>
-            {/* Pass setActivePage to Sidebar */}
-            <AppSidebar />
-            
-                <Header />
-                <main className="flex-1 overflow-auto p-6 mt-16">
-                    <p>lorem1000</p>
-                </main>
+        {/* Pass setActivePage to Sidebar */}
+        <AppSidebar />
+        <SidebarInset>
+          <Header />
+          <main className="flex-1 overflow-auto mt-10">
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nulla sapiente necessitatibus molestias, iste hic reiciendis ullam, dicta distinctio dolores impedit, dignissimos odio debitis! Tempora placeat provident ea sunt iure quibusdam, soluta doloremque sit nemo et veritatis obcaecati nesciunt iste cum! Tempore, totam. Itaque, officia nemo, cum explicabo cupiditate placeat, beatae totam doloribus numquam similique voluptatibus? Necessitatibus sunt impedit facilis! Reiciendis, dolore eaque. Cupiditate a iste ad commodi ipsum, sequi labore excepturi dignissimos odit assumenda iusto quaerat, explicabo autem eaque. Enim fuga asperiores repellat laudantium perspiciatis mollitia aspernatur vitae! Eius odit culpa in ad fuga, voluptatibus repudiandae amet reprehenderit et perspiciatis.</p>
+          </main>
+                </SidebarInset>
         </SidebarProvider>
     )
 }
