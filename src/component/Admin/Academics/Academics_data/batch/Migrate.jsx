2083c74e38d99bdef1f4de26bc4a084e7fb4320c
bhumi-choudhary <bhumichoudhary200@gmail.com>
2025-04-09 17:15:15 +0530

    newupd

---
+++ src/component/Admin/Academics/Academics_data/batch/Migrate.jsx
@@ -0,0 +1,14 @@
+import { Button } from '../../../../src/components/ui/button'
+import React from 'react'
+
+const Migrate = () => {
+    return (
+        <div>
+            <button className=" mt-4 w-full bg-gradient-to-r from-blue-600 to-indigo-400 text-white py-2 px-8 flex items-center  rounded-xl hover:from-blue-400 hover:to-indigo-700 transition-all whitespace-nowrap">
+                Migrate
+            </button>
+        </div>
+    )
+}
+
+export default Migrate
