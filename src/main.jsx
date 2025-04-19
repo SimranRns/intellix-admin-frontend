060c70f7a23e25efa8c34de29c3e3aebec8db644
bhumi-choudhary <bhumichoudhary200@gmail.com>
2025-04-19 16:30:05 +0530

    new

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,15 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { ThemeProvider } from './component/Admin/Dashboard/ThemeContext';
+import { Provider } from '@radix-ui/react-tooltip';
+import { store } from './component/Admin/Store/Store';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <ThemeProvider>
-    <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </ThemeProvider>
   </StrictMode>,
 )
